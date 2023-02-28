bdc2bb0e56af9972c0f019e6ece7e326b9c81375
choxkri <1059788@hr.nl>
2023-02-28 13:41:27 +0100

    ga verder met Player.cs methods "GetLevel" en "IsAlive"

---
+++ Castle Battle/Monster.cs
@@ -0,0 +1,15 @@
+﻿class Monster
+{
+    public string Name;
+    public int HP;
+    public int Strength;
+    public int Experience;
+
+    public Monster(string name, int hP, int strength, int experience)
+    {
+        this.Name = name;
+        this.HP = hP;
+        this.Strength = strength;
+        this.Experience = experience;
+    }
+}
---
+++ Castle Battle/Player.cs
@@ -0,0 +1,17 @@
+﻿class Player
+{
+    public string Name;
+    public int MaxHP;
+    public int Strength;
+    public int CurrentHP;
+    public int Experience;
+
+    public Player(string name, int baseHP, int baseStrength)
+    {
+        this.Name = name;
+        this.MaxHP = baseHP;
+        this.CurrentHP = baseHP;
+        this.Strength = baseStrength;
+        this.Experience = 0;
+    }
+}
