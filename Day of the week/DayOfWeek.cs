bf48f1ca3873cb5d30fbf2591271f476d2fa4af5
choxkri <1059788@hr.nl>
2023-02-28 13:10:29 +0100

    day of week update

--- Day of the week/DayOfWeek.cs
+++ Day of the week/DayOfWeek.cs
@@ -1,4 +1,56 @@
 ﻿class DayOfWeek
 {
+    public int index;
 
+    public DayOfWeek(int i)
+    { 
+        this.index = i; 
+    }
+
+    public string IndexToDay(int i)
+    {
+
+        if (i > 6)
+        {
+            while (i > 6) 
+            {
+                i -= 7;
+            }
+        }
+
+        else if (i < 0)
+        {
+            while (i < 0)
+            {
+                i += 7;
+            }
+        }
+
+        switch (i)
+        {
+            case 0:
+                return "Monday";
+
+            case 1:
+                return "Tuesday";
+
+            case 2:
+                return "Wednesday";
+
+            case 3:
+                return "Thursday";
+
+            case 4:
+                return "Friday";
+
+            case 5:
+                return "Saturday";
+
+            case 6:
+                return "Sunday";
+
+            default:
+                return null;
+        }
+    }
 }
