4efd10a9f9937edd3a70e9f25b5f234dd5073f2e
choxkri <1059788@hr.nl>
2023-02-28 12:48:45 +0100

    started with DayOfWeek opdr

--- Day of the week/Program.cs
+++ Day of the week/Program.cs
@@ -1,2 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//started testing with github
+
+using System;
+
+class Program
+{
+    public static void Main()
+    {
+        TestIndexToDay();
+
+        TestDayOfWeekInstance(10);
+        TestDayOfWeekInstance(-1);
+    }
+
+    private static void TestIndexToDay()
+    {
+        Console.WriteLine("Testing IndexToDay");
+        for (int i = -7; i < 7; i++)
+        {
+            Console.WriteLine($"Index: {i}; Day: {DayOfWeek.IndexToDay(i)}");
+        }
+        Console.WriteLine();
+    }
+
+    private static void TestDayOfWeekInstance(int startIndex)
+    {
+        Console.WriteLine($"Testing class object with index {startIndex}");
+        DayOfWeek dayOfWeek = new(startIndex);
+        List<string> days = new();
+        for (int i = -7; i < 7; i++)
+        {
+            days.Add(dayOfWeek.IsWeekend()
+                ? dayOfWeek.CurrentDay() + " :)"
+                : dayOfWeek.CurrentDay());
+            dayOfWeek.NextDay();
+        }
+
+        foreach (var day in days)
+            Console.WriteLine(day);
+    }
+}
