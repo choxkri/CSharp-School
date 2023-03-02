75a77049c54c653db7bc93223ea7f4dc7979b900
choxkri <1059788@hr.nl>
2023-03-02 19:53:10 +0100

    learned more about the Fisher-Yates algorithm, try to implement this in the f1 assignment tomorrow

--- F1 Season 2022/Season.cs
+++ F1 Season 2022/Season.cs
@@ -1,4 +1,23 @@
 ﻿class Season
 {
+    public int Year;
+    public List<Race> Races;
+    public List<Team> Teams;
 
+    public Season(int year, List<Race> races, List<Team> teams)
+    {
+        this.Year = year;
+        this.Races = races;
+        this.Teams = teams;
+    }
+
+    public void RunSeason()
+    {
+
+    }
+
+    public void SeasonResults()
+    {
+
+    }
 }
