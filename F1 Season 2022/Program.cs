c7c6383719a3c8182119c80ce3487860944f1f38
choxkri <1059788@hr.nl>
2023-03-04 17:31:06 +0100

    see how you can link a team to a driver without editing Program.cs

--- F1 Season 2022/Program.cs
+++ F1 Season 2022/Program.cs
@@ -1,102 +1,74 @@
-﻿using System;
-using System.Collections.Generic;
-
-class Program
+﻿class Program
 {
-    static void Main(string[] args)
+    public static void Main(string[] args)
     {
-        List<string> contenders = new List<string>()
-        {
-            "Contender 1",
-            "Contender 2",
-            "Contender 3",
-            "Contender 4",
-            "Contender 5",
-            "Contender 6",
-            "Contender 7",
-            "Contender 8",
-            "Contender 9",
-            "Contender 10"
-        };
+        var races = AssembleRaces2022();
+        var teams = AssembleTeams2022();
 
-        Dictionary<string, List<int>> scores = new Dictionary<string, List<int>>();
-
-        // Loop for 3 rounds
-        for (int round = 1; round <= 3; round++)
-        {
-            Console.WriteLine($"Round {round}");
-
-            // Shuffle contenders for this round
-            Shuffle(contenders);
-
-            // Assign scores for each contender
-            for (int i = 0; i < contenders.Count; i++)
-            {
-                string contender = contenders[i];
-
-                // Create a list of scores for this contender if it doesn't exist
-                if (!scores.ContainsKey(contender))
-                {
-                    scores[contender] = new List<int>();
-                }
-
-                // Assign score based on position
-                int score;
-                switch (i)
-                {
-                    case 0:
-                        score = 5;
-                        break;
-                    case 1:
-                        score = 4;
-                        break;
-                    case 2:
-                        score = 3;
-                        break;
-                    case 3:
-                        score = 2;
-                        break;
-                    default:
-                        score = 1;
-                        break;
-                }
-
-                // Add score to list for this contender
-                scores[contender].Add(score);
-
-                Console.WriteLine($"{contender} scored {score}");
-            }
-
-            Console.WriteLine();
-        }
-
-        // Print final scores for each contender
-        Console.WriteLine("Final Scores:");
-        foreach (string contender in contenders)
-        {
-            Console.Write($"{contender}: ");
-
-            List<int> contenderScores = scores[contender];
-            foreach (int score in contenderScores)
-            {
-                Console.Write($"{score} ");
-            }
-
-            Console.WriteLine();
-        }
+        Season season2022 = new(2022, races, teams);
+        season2022.RunSeason();
+        season2022.SeasonResults();
     }
 
-    static void Shuffle<T>(List<T> list)
+    public static List<Race> AssembleRaces2022() => new() {
+        new Race("Bahrain"),
+        new Race("Saudi Arabia"),
+        new Race("Australia"),
+        new Race("Emilia Romagna"),
+        new Race("Miami"),
+        new Race("Spain"),
+        new Race("Monaco"),
+        new Race("Azerbaijan"),
+        new Race("Canada"),
+        new Race("Great Britain"),
+        new Race("Austria"),
+        new Race("France"),
+        new Race("Hungary"),
+        new Race("Belgium"),
+        new Race("Netherlands"),
+        new Race("Italy"),
+        new Race("Singapore"),
+        new Race("Japan"),
+        new Race("United States"),
+        new Race("Mexico"),
+        new Race("Brazil"),
+        new Race("Abu Dhabi"),
+    };
+
+    public static List<Team> AssembleTeams2022()
     {
-        Random random = new Random();
-        int n = list.Count;
-        while (n > 1)
-        {
-            n--;
-            int k = random.Next(n + 1);
-            T value = list[k];
-            list[k] = list[n];
-            list[n] = value;
-        }
+        Team redbull = new("Red Bull Racing");
+        redbull.ContractDriver(new Driver("Max Verstappen"));
+        redbull.ContractDriver(new Driver("Sergio Perez"));
+        Team ferrari = new("Ferrari");
+        ferrari.ContractDriver(new Driver("Charles Leclerc"));
+        ferrari.ContractDriver(new Driver("Carlos Sainz Jr."));
+        Team mercedes = new("Mercedes-AMG");
+        mercedes.ContractDriver(new Driver("Louis Hamilton"));
+        mercedes.ContractDriver(new Driver("George Russel"));
+        Team alpine = new("Alpine-Renault");
+        alpine.ContractDriver(new Driver("Fernando Alonso"));
+        alpine.ContractDriver(new Driver("Esteban Ocon"));
+        Team mclaren = new("McLaren-Mercedes");
+        mclaren.ContractDriver(new Driver("Daniel Ricciardo"));
+        mclaren.ContractDriver(new Driver("Lando Norris"));
+        Team alfaRomeo = new("Alfa Romeo Racing-Ferrari");
+        alfaRomeo.ContractDriver(new Driver("Valtteri Bottas"));
+        alfaRomeo.ContractDriver(new Driver("Guanyu Zhou"));
+        Team astonMartin = new("Aston Martin-Mercedes");
+        astonMartin.ContractDriver(new Driver("Sebastian Vettel"));
+        astonMartin.ContractDriver(new Driver("Lance Stroll"));
+        Team haas = new("Haas-Ferrari");
+        haas.ContractDriver(new Driver("Mick Schumacher"));
+        haas.ContractDriver(new Driver("Kevin Magnussen"));
+        Team alphatauri = new("AlphaTauri-Red Bull");
+        alphatauri.ContractDriver(new Driver("Pierre Gasly"));
+        alphatauri.ContractDriver(new Driver("Yuki Tsunoda"));
+        Team williams = new("Williams-Mercedes");
+        williams.ContractDriver(new Driver("Alex Albon"));
+        williams.ContractDriver(new Driver("Nicholas Latifi"));
+
+        return new() { redbull, ferrari, mercedes, alpine, mclaren,
+            alfaRomeo, astonMartin, haas, alphatauri, williams };
     }
 }
--- F1 Season 2022/Season.cs
+++ F1 Season 2022/Season.cs
@@ -3,21 +3,54 @@
     public int Year;
     public List<Race> Races;
     public List<Team> Teams;
+    public List<Driver> Drivers;
+    Dictionary<string, List<int>> Scores;
 
     public Season(int year, List<Race> races, List<Team> teams)
     {
         this.Year = year;
         this.Races = races;
         this.Teams = teams;
+        this.Drivers = new List<Driver>();
+        this.Scores = new Dictionary<string, List<int>>();
     }
 
     public void RunSeason()
     {
+        foreach (Team team in this.Teams)
+        {
+            foreach(Driver driver in team.Drivers)
+            {
+                Scores[driver.Name] = new List<int>();
+                this.Drivers.Add(driver);
+            }
+        }
 
+        foreach (Race race in this.Races)
+        {
+            Shuffle(this.Drivers);
+            Console.WriteLine($"{this.Drivers[0].Name} of {}");
+        }
     }
 
     public void SeasonResults()
     {
 
+    }
+
+
+
+    static void Shuffle<T>(List<T> list)
+    {
+        Random random = new Random();
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = random.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
     }
 }
