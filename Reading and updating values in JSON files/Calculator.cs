6a72d16dec824f1dd652002c04e3f9e34f520d76
choxkri <1059788@hr.nl>
2023-03-06 10:10:25 +0100

    Finished json

---
+++ Reading and updating values in JSON files/Calculator.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+static class Calculator
+{
+    public static double Add(double x, double y)
+    {
+        return x + y;
+    }
+
+    public static double Subtract(double x, double y)
+    {
+        return x - y;
+    }
+
+    public static double Multiply(double x, double y)
+    {
+        return x * y;
+    }
+
+    public static double Divide(double x, double y)
+    {
+        return x / y;
+    }
+
+    public static double Modulo(double x, double y)
+    {
+        return x % y;
+    }
+
+    public static void StoreInMemory(double x)
+    {
+        StreamWriter writer = new StreamWriter("Memory.json"); //fileName is "Memory.json"
+        writer.Write(JsonConvert.SerializeObject(x));
+        writer.Close();
+        writer.Dispose();
+    }
+
+    public static double RestoreFromMemory()
+    {
+        StreamReader reader = new StreamReader("Memory.json"); //fileName is "Memory.json"
+        string jsonString = reader.ReadToEnd();
+        reader.Close();
+        reader.Dispose();
+        var fromjson = JsonConvert.DeserializeObject<double>(jsonString)!;
+        return fromjson;
+    }
+}
---
+++ Reading and updating values in JSON files/Program.cs
@@ -0,0 +1,8 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Calculator.StoreInMemory(2.0);
+        Console.WriteLine(Calculator.RestoreFromMemory());
+    }
+}
