c5b7b50c0893144fca7267ffadf4a92befeb8983
choxkri <1059788@hr.nl>
2023-03-06 13:02:54 +0100

    started on c5

---
+++ Reading and updating objects in JSON files/Car.cs
@@ -0,0 +1,9 @@
+﻿class Car
+{
+    public string Brand;
+    public int Mileage;
+
+    public Car(string brand) => Brand = brand;
+
+    public void Drive() => Mileage++;
+}
---
+++ Reading and updating objects in JSON files/Program.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    public static void Main()
+    {
+        //Your code here
+        StreamReader reader = new("Cars.json");
+        string File2Json = reader.ReadToEnd();
+        List<Car> listOfObjects = JsonConvert.DeserializeObject<List<Car>>(File2Json)!;
+        reader.Close();
+        reader.Dispose();
+
+        for (int i = 0; i < 5; i++) 
+        {
+            foreach (Car car in listOfObjects) 
+            {
+                car.Drive();
+            }
+        }
+
+        StreamWriter writer = new("Cars.json");
+        string List2Json = JsonConvert.SerializeObject(listOfObjects);
+        writer.Write(List2Json);
+        writer.Close();
+        writer.Dispose();
+            
+    }
+}
