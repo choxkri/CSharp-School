2545d54626b6bf53eea7a6f04d6d8e67baf4c8af
choxkri <1059788@hr.nl>
2023-03-13 09:46:34 +0100

    finished c5

--- Classes with objects for fields and JSON files/Program.cs
+++ Classes with objects for fields and JSON files/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿using Newtonsoft.Json;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        StreamReader reader = new("People.json");
+        string File2Json = reader.ReadToEnd();
+        List<Person> listOfObjects = JsonConvert.DeserializeObject<List<Person>>(File2Json)!;
+        reader.Close();
+
+        for (int i = 0; i < listOfObjects.Count; i++) 
+        {
+            foreach (Car car in listOfObjects[i].OwnedCars) 
+            {
+                for (int x = 0; x < 5; x++)
+                {
+                    car.Drive();
+                }
+            }
+        }
+
+        StreamWriter writer = new("People.json");
+        string List2Json = JsonConvert.SerializeObject(listOfObjects);
+        writer.Write(List2Json);
+        writer.Close();
+    }
+}
