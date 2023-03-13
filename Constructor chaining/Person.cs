fbccad1a93935efc2fdbe4da87013e45dda85c08
choxkri <1059788@hr.nl>
2023-03-13 11:22:30 +0100

    finished constructor overloading and chaining

---
+++ Constructor chaining/Person.cs
@@ -0,0 +1,16 @@
+﻿public class Person
+{
+    public string Name;
+    public int Age;
+
+    public Person(string name) : this(name, 0)
+    {
+
+    }
+
+    public Person(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+}
---
+++ Constructor overloading/Person.cs
@@ -0,0 +1,17 @@
+﻿class Person
+{
+    public string Name;
+    public int Age;
+
+    public Person(string name)
+    {
+        this.Name = name;
+        this.Age = 0;
+    }
+
+    public Person(string name, int age)
+    {
+        this.Name = name;
+        this.Age = age;
+    }
+}
