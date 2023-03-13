56e869f67774a33158ac458ba692646ab8d5f240
choxkri <1059788@hr.nl>
2023-03-13 11:31:05 +0100

    finished overloading default constructor

---
+++ Overloading default and copy constructor/Point.cs
@@ -0,0 +1,26 @@
+﻿class Point
+{
+    public double x;
+    public double y;
+
+    public Point()
+    {
+
+    }
+    public Point(double num1, double num2)
+    {
+        this.x = num1;
+        this.y = num2;
+    }
+
+    public Point(Point p) : this(p.x, p.y)
+    {
+
+    }
+
+    public static double EuclideanDistance(Point p1, Point p2)
+    {
+        double result = Math.Sqrt(Math.Pow(p2.x - p1.x, 2) + Math.Pow(p2.y - p1.y, 2));
+        return result;
+    }
+}
