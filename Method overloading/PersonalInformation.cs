6a6795db875192e1c675388b6693ec4c4ba4d638
choxkri <1059788@hr.nl>
2023-03-13 10:56:21 +0100

    finished method overloading assignment

---
+++ Method overloading/PersonalInformation.cs
@@ -0,0 +1,18 @@
+﻿static class PersonalInformation
+{
+
+    public static void PrintName(string name)
+    {
+        Console.WriteLine(name);
+    }
+
+    public static void PrintName(string fn, string ln)
+    {
+        Console.WriteLine($"{fn} {ln}");
+    }
+
+    public static void PrintName(char n, string ln)
+    {
+        Console.WriteLine($"{n}. {ln}");
+    }
+}
---
+++ Method overloading/Program.cs
@@ -0,0 +1,9 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        PersonalInformation.PrintName('c', "bouchrit");
+        PersonalInformation.PrintName("choukri", "bouchrit");
+        PersonalInformation.PrintName("choukri");
+    }
+}
