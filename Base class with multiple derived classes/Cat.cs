3b378a5c3c7eea4a43cc21aa99aaf8bf791f6376
choxkri <1059788@hr.nl>
2023-03-15 15:36:22 +0100

    finished base class with multiple derived classes and started on derived and derived further

---
+++ Base class with multiple derived classes/Cat.cs
@@ -0,0 +1,12 @@
+﻿class Cat : Animal
+{
+    public Cat(string n) : base(n) 
+    {
+        this.Sound = "Meow!";
+    }
+
+    public void Climb()
+    {
+        Console.WriteLine($"{this.Name} climbs the curtains");
+    }
+}
---
+++ Base class with multiple derived classes/Dog.cs
@@ -0,0 +1,12 @@
+﻿class Dog : Animal
+{
+    public Dog(string n) : base(n)
+    {
+        this.Sound = "Woof!";
+    }
+
+    public void Fetch()
+    {
+        Console.WriteLine($"{this.Name} fetches the stick");
+    }
+}
