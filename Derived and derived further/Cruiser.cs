a5565b3d565f0638a09864864e57a0dd6060b275
choxkri <1059788@hr.nl>
2023-03-15 20:50:09 +0100

    finished assignment derived further and further.

---
+++ Derived and derived further/Cruiser.cs
@@ -0,0 +1,14 @@
+﻿class Cruiser : Motorcycle
+{
+
+    public int SeatHeight;
+    public Cruiser(string ma, string mo, int ye, int height) : base(ma, mo, ye)
+    {
+        this.SeatHeight = height;
+    }
+
+    public override string Ride()
+    {
+        return $"{base.Ride()} with a seat height of {this.SeatHeight} cm";
+    }
+}
---
+++ Derived and derived further/HarleyDavidson.cs
@@ -0,0 +1,12 @@
+﻿class HarleyDavidson : Cruiser
+{
+    public HarleyDavidson(string mo, int ye) : base("Harley Davidson", mo, ye, 68)
+    {
+
+    }
+
+    public override string Ride()
+    {
+        return $"{base.Ride()} and with pride.";
+    }
+}
