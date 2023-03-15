0428301c0e31ec2d452043ed6ad29e97867ba6b0
choxkri <1059788@hr.nl>
2023-03-15 10:39:20 +0100

    finished w04 assignments

---
+++ Library/Book.cs
@@ -0,0 +1,20 @@
+﻿class Book
+{
+    public int ID;
+    public string Title;
+
+    public Book(int id) : this(id, "Title unknown")
+    {
+
+    }
+    public Book(int id, string title)
+    {
+        this.ID = id;
+        this.Title = title;
+    }
+
+    public string Info()
+    {
+        return $"ID = {this.ID}, Title = {this.Title}";
+    }
+}
---
+++ Library/Library.cs
@@ -0,0 +1,52 @@
+﻿class Library
+{
+    public List<Book> Books;
+
+    public Library(List<Book> books)
+    {
+        this.Books = books;
+    }
+
+    public Library()
+    {
+        this.Books = new List<Book>();
+    }
+
+    public void AddBook(Book book)
+    {
+        this.Books.Add(book);
+    }
+
+    public void AddBook(int id, string title)
+    {
+        Book book = new(id, title);
+        this.Books.Add(book);
+    }
+
+    public Book FindBook(int id)
+    {
+        foreach (Book book in this.Books) 
+        {
+            if (book.ID == id) 
+                return book;
+        }
+
+        return null;
+    }
+
+    public Book FindBook(string id)
+    {
+        if (id.All(char.IsDigit))
+        {
+            int term = Convert.ToInt32(id);
+            return this.FindBook(term);
+        }
+
+        else
+        {
+            Console.WriteLine($"id = {id}: not a valid book id. input string was not in a correct format.");
+            return null;
+        }
+    }
+
+}
