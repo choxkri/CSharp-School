2a92d4f01f99b4f5ff42dc6a56df12558d0eab24
choxkri <1059788@hr.nl>
2023-03-15 11:57:14 +0100

    Finished keyword is, object looping and casting

---
+++ Keyword is, object looping and casting/Person.cs
@@ -0,0 +1,7 @@
+﻿class Person
+{
+    public string Name;
+    public Person(string name) => Name = name;
+
+    public string Introduce() => $"I am {Name}";
+}
---
+++ Keyword is, object looping and casting/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static void Main()
+    {
+        Person john = new("John Doe");
+        Student jane = new("Jane Doe");
+        //Write your code here
+        List<Person> list = new()
+        {
+            john,
+            jane
+        };
+
+        foreach (Person person in list)
+        {
+            Console.WriteLine(person.Introduce());
+            if (person is Student)
+            {
+                Console.WriteLine(((Student)person).Status());
+            }
+        }
+    }
+
+}
