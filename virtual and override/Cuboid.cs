abeb3a632ba7d5b6a43cbd0e14d0accafe2939ee
choxkri <1059788@hr.nl>
2023-03-15 15:07:36 +0100

    finished virtual and override assignment

---
+++ virtual and override/Cuboid.cs
@@ -0,0 +1,29 @@
+﻿class Cuboid : Rectangle
+{
+    public int Height;
+    public Cuboid(int length, int width, int height) : base(length, width)
+    {
+        this.Height = height;
+    }
+
+    public int Volume()
+    {
+        return this.Height * this.Width * this.Length;
+    }
+
+    public override int Area()
+    {
+        return 2 * (this.Length * this.Width + this.Length * this.Height + this.Width * this.Height);
+    }
+
+    public override int Perimeter()
+    {
+        return 4 * (this.Length + this.Width + this.Height);
+    }
+
+    public override string Info()
+    {
+        return base.Info() + $"; Height: {this.Height}";
+    }
+
+}
