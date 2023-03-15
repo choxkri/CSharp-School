7a51f368dd9ee0213f2b2a16e3882ba2bb0ccfdc
choxkri <1059788@hr.nl>
2023-03-15 10:54:24 +0100

    finished creating objects of inherited classes

---
+++ Creating objects of inherited classes/Program.cs
@@ -0,0 +1,13 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Person john = new("John Doe");
+        Student jane = new("Jane Doe");
+        Console.WriteLine(john.Introduce());
+        Console.WriteLine(jane.Introduce());
+        Console.WriteLine(jane.Status());
+        jane.Graduate();
+        Console.WriteLine(jane.Status());
+    }
+}
