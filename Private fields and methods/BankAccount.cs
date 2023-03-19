aee8dc894b5f4bef58d54f0a37a75bafe34bf808
choxkri <1059788@hr.nl>
2023-03-19 17:16:05 +0100

    finished private fields and methods assignment

---
+++ Private fields and methods/BankAccount.cs
@@ -0,0 +1,49 @@
+﻿class BankAccount
+{
+    private double _balance;
+
+    public BankAccount(double balance)
+    {
+        this._balance = balance;
+    }
+
+    public double ReadBalance() 
+    { 
+        return _balance;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0) 
+        {
+            this._balance += amount;
+        }
+    }
+
+    public double Withdraw(double amount) 
+    {
+        bool check = SufficientBalance(amount);
+        if (check) 
+        {
+            this._balance -= amount;
+            return this._balance;
+        }
+
+        if (amount < 0 | check == false) 
+        {
+            return 0;
+        }
+
+        return 0;
+    }
+
+    private bool SufficientBalance(double amount) 
+    {
+        if (this._balance < amount) 
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
