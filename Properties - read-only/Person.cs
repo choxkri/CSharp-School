c396f45a762fcecdc026020ae65a14b8ca89225c
choxkri <1059788@hr.nl>
2023-03-19 17:56:35 +0100

    finished Properties - read-only assignment

---
+++ Properties - read-only/Person.cs
@@ -0,0 +1,15 @@
+﻿class Person
+{
+    private readonly string _firstName;
+    private readonly string _lastName;
+    public Person(string fn, string ln)
+    {
+        this._firstName = fn;
+        this._lastName = ln;
+    }
+
+    public string FullName
+    {
+        get { return $"{_firstName} {_lastName}"; }
+    }
+}
