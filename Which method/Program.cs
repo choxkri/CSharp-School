3c8d25fa8288529f8bfc92ab035f309f491500d7
choxkri <1059788@hr.nl>
2023-03-19 11:00:12 +0100

    finished which method assignment

---
+++ Which method/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        //Create an instance of SevenElevenVariant with a seed of 0 here.
+        //Then call its only public method.
+        SevenElevenVariant test = new(0);
+        test.PlayGame();
+
+    }
+}
