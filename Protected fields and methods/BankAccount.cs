a4b34a30876258588185802317a5aeb4c37266fa
choxkri <1059788@hr.nl>
2023-03-19 17:41:47 +0100

    codegrade tripping with unit test protected fields and methods

---
+++ Protected fields and methods/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿class BankAccount
+{
+    private double _balance;
+    protected int YearsPassed;
+    protected double InterestRate;
+
+    public BankAccount(double balance, double interest)
+    {
+        this._balance = balance;
+        this.InterestRate = interest;
+        this.YearsPassed = 0;
+    }
+
+    public double ReadBalance()
+    {
+        return _balance;
+    }
+
+    public void Deposit(double amount)
+    {
+        if (amount > 0)
+        {
+            this._balance += amount;
+        }
+    }
+
+    public virtual double Withdraw(double amount)
+    {
+        bool check = SufficientBalance(amount);
+        if (check)
+        {
+            this._balance -= amount;
+            return this._balance;
+        }
+
+        if (amount < 0 | check == false)
+        {
+            return 0;
+        }
+
+        return 0;
+    }
+
+    private bool SufficientBalance(double amount)
+    {
+        if (this._balance < amount)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected void ApplyInterest()
+    {
+        this._balance += this._balance * this.InterestRate;
+    }
+
+    public virtual void NextYear()
+    {
+        ApplyInterest();
+    }
+}
---
+++ Protected fields and methods/SavingsAccount.cs
@@ -0,0 +1,33 @@
+﻿class SavingsAccount : BankAccount
+{
+    private bool _locked;
+
+    public SavingsAccount(double balance, double interest) : base(balance, interest)
+    {
+        this._locked = true;
+    }
+
+    public override double Withdraw(double amount)
+    {
+        if (this._locked)
+        {
+            return 0;
+        }
+        
+        return base.Withdraw(amount);
+    }
+
+    public override void NextYear()
+    {
+        this.YearsPassed++;
+        if (this.YearsPassed < 5)
+        {
+            base.NextYear();
+        }
+
+        else
+        {
+            this._locked = false;
+        }
+    }
+}
