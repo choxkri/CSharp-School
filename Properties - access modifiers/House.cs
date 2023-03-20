ea595292936862d34f12afb76d7dd1c52ea7ba30
choxkri <1059788@hr.nl>
2023-03-20 10:10:22 +0100

    finished property problem

---
+++ Properties - access modifiers/House.cs
@@ -0,0 +1,58 @@
+﻿public class House
+{
+    public House(double squareMeters, int numberOfRooms)
+        : this(squareMeters, numberOfRooms, false, false) { }
+    public House(double squareMeters, int numberOfRooms,
+        bool isFurnished, bool hasGarden)
+    {
+        SquareMeters = squareMeters;
+        NumberOfRooms = numberOfRooms;
+        IsFurnished = isFurnished;
+        HasGarden = hasGarden;
+    }
+
+    public double SquareMeters 
+    {
+        get
+        {
+            return SquareMeters;
+        }
+        set
+        {
+
+        }
+    }
+    protected int NumberOfRooms { get; set; }
+    private bool IsFurnished { get; set; }
+    private bool HasGarden { get; set; }
+
+    public double Price
+    {
+        get
+        {
+            double basePrice = SquareMeters * 1000;
+            double roomPrice = NumberOfRooms * 50000;
+            double furnishedPrice = IsFurnished ? 10000 : 0;
+            double gardenPrice = HasGarden ? 25000 : 0;
+            return basePrice + roomPrice + furnishedPrice + gardenPrice;
+        }
+    }
+
+    public string Info
+    {
+        get
+        {
+            return $"Square meters: {SquareMeters}\n"
+            + $"Rooms: {NumberOfRooms}\n"
+            + $"Furnished: {(IsFurnished ? "yes" : "no")}\n"
+            + $"Garden: {(HasGarden ? "yes" : "no")}";
+        }
+    }
+
+    public virtual void HomeImprovement()
+    {
+        SquareMeters += 10;
+        IsFurnished = true;
+        HasGarden = true;
+    }
+}
---
+++ Property Problem/Person.cs
@@ -0,0 +1,27 @@
+﻿class Person
+{
+    public Person(string name, int age)
+    {
+        Name = name;
+        if (0 <= age && age <= 130)
+        {
+            _age = age;
+        }
+
+        else
+        {
+            _age = 0;
+        }
+    }
+
+    public string Name { get; set; }
+
+    private int _age;
+    public int Age
+    {
+        get => _age;
+        set => _age = (value >= 0 && value <= 130) ? value : 0;
+    }
+
+    public string Info { get => $"{Name} is {Age} years old"; }
+}
