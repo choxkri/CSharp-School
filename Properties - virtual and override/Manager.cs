f3f66dd668396d0ca6c488623f6fe5f4d9e08aa0
choxkri <1059788@hr.nl>
2023-03-20 09:48:26 +0100

    finished properties virtual and override

---
+++ Properties - virtual and override/Manager.cs
@@ -0,0 +1,13 @@
+﻿public class Manager : Employee
+{
+    public Manager(string name, double salary) : base(name, salary) { }
+    public Manager(Employee employee)
+        : this(employee.Name, employee.Salary + 1000) { }
+
+    public int TeamSize { get; set; }
+
+    public override string Info
+    {
+        get => base.Info + $"; Team Size: {TeamSize}";
+    }
+}
