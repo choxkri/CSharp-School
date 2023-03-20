6242c0ab6ad8306ed05c3f73914a59ac19a1f3b8
choxkri <1059788@hr.nl>
2023-03-20 18:32:15 +0100

    finished interfaces inheriting from other interfaces

---
+++ Interfaces inheriting from other interfaces/IStorable.cs
@@ -0,0 +1,14 @@
+﻿interface IStorable
+{
+    public string FileName { get; set; }
+
+    public void Load()
+    {
+        return;
+    }
+
+    public void Save()
+    {
+        return;
+    }
+}
