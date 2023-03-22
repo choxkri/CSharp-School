bd0adc0de6826c718170259a9cfd4a79a9a7eacb
choxkri <1059788@hr.nl>
2023-03-22 10:39:59 +0100

    finished create abstract class

---
+++ Create abstract class/Person.cs
@@ -0,0 +1,20 @@
+﻿public abstract class Person
+{
+    public int Age { get; set; }
+    public abstract string FirstName { get; set; }
+    public abstract string LastName{ get; set; }
+
+    protected Person(string fn, string ln, int age)
+    {
+        this.Age = age;
+        this.FirstName = fn;
+        this.LastName = ln;
+    }
+
+    public abstract string Greet();
+
+    public virtual string GetFullName()
+    {
+        return $"{this.FirstName} {this.LastName}";
+    }
+}
