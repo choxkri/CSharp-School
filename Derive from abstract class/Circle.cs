a4b81873a29722668b74b876ab5e74dafcd0f849
choxkri <1059788@hr.nl>
2023-03-22 11:29:37 +0100

    finished derive from abstract class

---
+++ Derive from abstract class/Circle.cs
@@ -0,0 +1,24 @@
+﻿public class Circle : Shape
+{
+    public double Radius { get; set; }
+    public Circle(double r)
+    {
+        this.Radius = r;
+    }
+
+    public override double Area
+    {
+        get { return Math.PI * Math.Pow(Radius, 2); }
+    }
+
+    public override double Perimeter
+    {
+        get { return 2 * (Radius * Math.PI); }
+    }
+
+    public override string Info()
+    {
+        return $"Circle with a radius of {this.Radius}";
+    }
+
+}
---
+++ Derive from abstract class/Square.cs
@@ -0,0 +1,24 @@
+﻿public class Square : Shape
+{
+    public double Length { get; set; }
+    public Square(double l)
+    {
+        this.Length = l;
+    }
+
+    public override double Area
+    {
+        get { return Math.Pow(Length, 2); }
+    }
+
+    public override double Perimeter
+    {
+        get { return 4 * Length; }
+    }
+
+    public override string Info()
+    {
+        return $"Circle with sides of {this.Length}";
+    }
+
+}
