0bc21e63b2eb353513ab2987ae0cb68486c73121
choxkri <1059788@hr.nl>
2023-03-25 23:53:58 +0100

    finished cast to abstract base class assignment

--- Cast to abstract base class/Program.cs
+++ Cast to abstract base class/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        List<Bill> list = new List<Bill>
+        {
+            new ElectricityBill(50, "John Doe"),
+            new ElectricityBill(75, "Jane Doe"),
+            new GasBill(100, "Bob Johnson", false),
+            new GasBill(125, "John Doe", true)
+        };
+        double total = 0;
+        foreach (Bill bill in list) 
+        {
+            Console.WriteLine(bill.GetDescription());
+            total += bill.Amount;
+        }
+        Console.WriteLine($"Total amount: {(int)total}");
+    }
+}
