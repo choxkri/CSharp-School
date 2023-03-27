466b764f9234a66aa76cd788cf5eb8e115e18b2c
choxkri <1059788@hr.nl>
2023-03-27 10:30:37 +0200

    finished using new methods assignment

---
+++ using new methods/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    public static void Main(string[] args)
+    {
+        Employee john = new("John", "Doe", "john.doe@example.com");
+        Manager jane = new("Jane", "Doe", "jane.doe@example.com", "Sales");
+        SalesPerson bob = new("Bob", "Smith", "bob.smith@example.com", 100000);
+        List<Employee> list = new List<Employee>()
+        {
+            john,
+            jane,
+            bob
+        };
+
+        foreach(Employee e in list) 
+        {
+            e.PrintEmployeeInfo();
+        }
+
+        foreach(Employee e in list)
+        {
+            if (e is Manager x) 
+            {
+                x.PrintEmployeeInfo();
+                continue;
+            }
+
+            if (e is SalesPerson y)
+            {
+                y.PrintEmployeeInfo();
+                continue;
+            }
+
+            e.PrintEmployeeInfo();
+        }
+    }
+}
