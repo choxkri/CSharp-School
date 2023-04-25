3dbf738612ae92331eb447822b71c8939113971c
choxkri <1059788@hr.nl>
2023-04-25 13:04:06 +0200

    finished generic methods

---
+++ Generic method/RewardGenerator.cs
@@ -0,0 +1,11 @@
+﻿public static class RewardGenerator
+{
+    public static Random rng = new Random(0);
+
+    public static T GetRandomElement<T>(List<T> list)
+    {
+
+        return list[rng.Next(list.Count-1)];
+        
+    }
+}
