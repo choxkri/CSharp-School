a2ee1a9369325be715982219159aca65cba60609
choxkri <1059788@hr.nl>
2023-04-25 13:15:16 +0200

    finished generic properties

---
+++ Generic properties/KeyValuePair.cs
@@ -0,0 +1,36 @@
+﻿public class KeyValuePair<T>
+{
+    private T _keysetter;
+    public T TKey
+    {
+        get
+        {
+            return _keysetter;
+        }
+
+        set
+        {
+            _keysetter = value;
+        }
+    }
+
+    private T _valuesetter;
+    public T TValue
+    {
+        get
+        {
+            return _valuesetter;
+        }
+
+        set
+        {
+            _valuesetter = value;
+        }
+    }
+
+    public KeyValuePair(T key, T value)
+    {
+        TKey = key;
+        TValue = value;
+    }
+}
