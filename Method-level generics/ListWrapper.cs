1bfa9e1647ddc0de94dd7d28e2c2140cb68b27fa
choxkri <1059788@hr.nl>
2023-05-03 14:56:31 +0200

    finisehd method level generics

---
+++ Method-level generics/ListWrapper.cs
@@ -0,0 +1,29 @@
+﻿public class ListWrapper<T>
+{
+    private List<T> _myList = new List<T>();
+
+    public void Add(T item)
+    {
+        _myList.Add(item);
+    }
+
+    public T Get(int index)
+    {
+        return _myList[index];
+    }
+
+    public int Count()
+    {
+        return _myList.Count;
+    }
+
+    public int HighestCount<T2>(ListWrapper<T2> wrapper)
+    {
+        if (wrapper.Count() > _myList.Count()) 
+        {
+            return wrapper.Count();
+        }
+
+        return _myList.Count();
+    }
+}
