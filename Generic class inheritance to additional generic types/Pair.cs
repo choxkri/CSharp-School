abd71ecc9bfa821001d9bbc0f287f1a8a244f94d
choxkri <1059788@hr.nl>
2023-05-03 15:43:23 +0200

    Finished generic class inheritance

---
+++ Generic class inheritance to additional generic types/Pair.cs
@@ -0,0 +1,38 @@
+﻿public class Pair<T1, T2>
+{
+    private T1 _fstSetter;
+
+    public T1 Fst
+    {
+        get
+        {
+            return _fstSetter;
+        }
+
+        private set 
+        {
+            _fstSetter = value;
+        }
+    }
+
+    private T2 _sndSetter;
+
+    public T2 Snd
+    {
+        get
+        {
+            return _sndSetter;
+        }
+
+        private set
+        {
+            _sndSetter = value;
+        }
+    }
+
+    public Pair(T1 fst, T2 snd)
+    {
+        this.Fst = fst;
+        this.Snd = snd;
+    }
+}
---
+++ Generic class inheritance to additional generic types/Triple.cs
@@ -0,0 +1,22 @@
+﻿public class Triple<T1, T2, T3> : Pair<T1, T2>
+{
+    private T3 _trdSetter;
+
+    public T3 Trd
+    {
+        get
+        {
+            return _trdSetter;
+        }
+
+        private set
+        {
+            _trdSetter = value;
+        }
+    }
+
+    public Triple(T1 fst, T2 snd, T3 trd) : base(fst, snd)
+    {
+        this.Trd = trd;
+    }
+}
