755732106358210448c1a16831d88749221296ef
choxkri <1059788@hr.nl>
2023-05-03 15:04:53 +0200

    finished default value of generic type

---
+++ Default value of generic type/Container.cs
@@ -0,0 +1,27 @@
+﻿public class Container<T>
+{
+    private T _valueSetter;
+
+    public T Value
+    {
+        get
+        {
+            return _valueSetter;
+        }
+
+        set 
+        { 
+            _valueSetter = value;
+        }
+    }
+
+    public Container(T value)
+    { 
+        this.Value = value;
+    }
+
+    public void ResetValue()
+    {
+        this.Value = default(T);
+    }
+}
