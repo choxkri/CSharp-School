0f9fd8667c2ca11dfd512c0ed81855e1b5420a33
choxkri <1059788@hr.nl>
2023-05-10 21:50:22 +0200

    started on yield

---
+++ yield return and IEnumerable/Range.cs
@@ -0,0 +1,17 @@
+﻿public class Range
+{
+    public int Start { get; }
+    public int End { get; }
+    public int Step { get; }
+
+    public Range(int start, int end, int step)
+    {
+        Start = start;
+        End = end;
+        Step = step;
+    }
+
+    public 
+
+
+}
