85392ccd9b78a27e6d88f6de68f56b510299b5ed
choxkri <1059788@hr.nl>
2023-05-14 22:11:14 +0200

    finished tuple create w11.01

---
+++ Tuple - create/EmployeeData.cs
@@ -0,0 +1,26 @@
+﻿public class EmployeeData
+{
+    private readonly List<Employee> _employees;
+
+    public EmployeeData(List<Employee> employees)
+    {
+        _employees = employees;
+    }
+
+    public (double, double) GetSalaryRange()
+    {
+        double minSalary = double.MaxValue;
+        double maxSalary = double.MinValue;
+
+        foreach (Employee employee in _employees)
+        {
+            if (employee.Salary < minSalary)
+                minSalary = employee.Salary;
+
+            if (employee.Salary > maxSalary)
+                maxSalary = employee.Salary;
+        }
+
+        return (minSalary, maxSalary);
+    }
+}
