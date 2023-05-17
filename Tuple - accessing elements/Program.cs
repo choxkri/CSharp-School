f9eac5d10286b4d35b0cca382dc9232cfe42e38b
choxkri <1059788@hr.nl>
2023-05-17 22:08:04 +0200

    finished tuple - accessing elements

---
+++ Tuple - accessing elements/Program.cs
@@ -0,0 +1,8 @@
+﻿public class Program
+{
+    public static void Main(string[] args)
+    {
+        var test = TestResultProcessor.GetTestResult(1, 2);
+        TestResultProcessor.PrintTestResult(test);
+    }
+}
---
+++ Tuple - accessing elements/TestResultProcessor.cs
@@ -0,0 +1,30 @@
+﻿public static class TestResultProcessor
+{
+    public static Tuple<double, bool> GetTestResult(int p, int maxp)
+    {
+        double grade = (double)p / maxp;
+        grade = grade * 10.0;
+        bool passed = false;
+        if (grade >= 5.5) 
+        { 
+            passed = true;
+        }
+        var result = Tuple.Create(grade, passed);
+        return result;
+    }
+
+    public static void PrintTestResult(Tuple<double, bool> result)
+    {
+        if (result.Item2 == true)
+        {
+            Console.WriteLine($"Grade: {result.Item1}");
+            Console.WriteLine("Passed");
+        }
+
+        else
+        {
+            Console.WriteLine($"Grade: {result.Item1}");
+            Console.WriteLine("Did not pass");
+        }
+    }
+}
