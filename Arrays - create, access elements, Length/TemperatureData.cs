ee81547bd444a754435f3eb1d31a1248de5394c2
choxkri <1059788@hr.nl>
2023-05-17 22:44:23 +0200

    finished Arrays - create, access elements, Length

---
+++ Arrays - create, access elements, Length/TemperatureData.cs
@@ -0,0 +1,20 @@
+﻿public class WeatherData
+{
+    //Change from List to array
+    private readonly double[] _data;
+
+    public int NumberOfReadings
+    {
+        get
+        {
+            return _data.Length;
+        }
+    }
+
+    public WeatherData(double[] data) => _data = data;
+
+    public double GetHighestTemperature()
+    {
+        return _data.Max();
+    }
+}
