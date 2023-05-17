f2f7b705cb3ac473b109e397185a029cf37334dd
choxkri <1059788@hr.nl>
2023-05-17 23:08:52 +0200

    finished Arrays - Copy

---
+++ Arrays - Copy/TemperatureData.cs
@@ -0,0 +1,61 @@
+﻿public class WeatherData
+{
+    //Change from List to array
+    private readonly double[] _data;
+
+    public int NumberOfReadings
+    {
+        get
+        {
+            return _data.Length;
+        }
+    }
+
+    public WeatherData(double[] data) => _data = data;
+
+    public double GetHighestTemperature()
+    {
+        return _data.Max();
+    }
+
+    public void Sort(bool order)
+    {
+        if (order)
+        {
+            Array.Sort(_data);
+        }
+
+        else
+        {
+            Array.Sort(_data);
+            Array.Reverse(_data);
+        }
+    }
+
+    public void PrintTemperatures()
+    {
+        foreach (double temp in _data)
+        {
+            Console.WriteLine(temp);
+        }
+    }
+
+    public double[] GetTemperaturesSorted(bool order)
+    {
+        double[] copy = new double[_data.Length];
+        if (order)
+        {
+            Array.Copy(_data, copy, _data.Length);
+            Array.Sort(copy);
+            return copy;
+        }
+
+        else
+        {
+            Array.Copy(_data, copy, _data.Length);
+            Array.Sort(copy);
+            Array.Reverse(copy);
+            return copy;
+        }
+    }
+}
