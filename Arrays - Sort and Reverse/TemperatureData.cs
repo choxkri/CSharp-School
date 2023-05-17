8a1c2019496350ca1fc34703d703d68843591a66
choxkri <1059788@hr.nl>
2023-05-17 22:55:15 +0200

    finished Arrays - Sort and Reverse

---
+++ Arrays - Sort and Reverse/TemperatureData.cs
@@ -0,0 +1,42 @@
+﻿public class WeatherData
+{
+    //Change from List to array
+    private readonly double[] _data;
+
+    public int NumberOfReadings
+    {
+        get
+        {
+            return _data.Length;
+        }
+    }
+
+    public WeatherData(double[] data) => _data = data;
+
+    public double GetHighestTemperature()
+    {
+        return _data.Max();
+    }
+
+    public void Sort(bool order)
+    {
+        if (order)
+        {
+            Array.Sort(_data);
+        }
+
+        else
+        {
+            Array.Sort(_data);
+            Array.Reverse(_data);
+        }
+    }
+
+    public void PrintTemperatures()
+    {
+        foreach (double temp in _data)
+        {
+            Console.WriteLine(temp);
+        }
+    }
+}
