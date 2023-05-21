82090b1fb60b35ae66e93b2f5e8b69fd72b93935
choxkri <1059788@hr.nl>
2023-05-21 13:05:41 +0200

    finished jagged arrays

---
+++ Jagged array - looping/SalesHelper.cs
@@ -0,0 +1,15 @@
+﻿public static class SalesHelper
+{
+    public static void PrintSalesData(int[][] data)
+    {
+        for (int i = 0; i < data.Length; i++)
+        {
+            Console.WriteLine($"Sales data for department {i + 1}:");
+            for (int j = 0; j < data[i].Length; j++)
+            {
+                Console.WriteLine($" - {data[i][j]}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
