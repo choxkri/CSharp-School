60b1656b2e72d290e3591fbfee2c186138f81edd
choxkri <1059788@hr.nl>
2023-05-21 22:17:40 +0200

    finished N-dimensional arrays - looping

---
+++ N-dimensional arrays - looping/GrayscaleImage.cs
@@ -0,0 +1,22 @@
+﻿public class GrayscaleImage
+{
+    public int Height { get; }
+    public int Width { get; }
+    //Set PixelData
+    public int[,] PixelData { get; }
+    public GrayscaleImage(int height, int width)
+    {
+        //Set Width, Height and PixelData
+        Height = height; 
+        Width = width;
+        PixelData = new int[Height, Width];
+    }
+
+    public GrayscaleImage(int[,] pixelData)
+    {
+        //Set Width, Height and PixelData
+        PixelData = pixelData;
+        Height = pixelData.GetLength(0);
+        Width = pixelData.GetLength(1);
+    }
+}
