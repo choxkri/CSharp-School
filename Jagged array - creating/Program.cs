05b6153bf6b63057676f3e4d3d8a56b609300ee9
choxkri <1059788@hr.nl>
2023-05-21 16:16:43 +0200

    started creating jagged arrays

---
+++ Jagged array - creating/Program.cs
@@ -0,0 +1,10 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("amount of deps:");
+        string input = Console.ReadLine();
+        int deps = Convert.ToInt32(input);
+
+    }
+}
