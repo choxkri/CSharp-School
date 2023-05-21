9b47666c220d43f27bb8f145c8d83783e51fa224
choxkri <1059788@hr.nl>
2023-05-21 23:03:04 +0200

    finished n-dimensional arrays - creating

---
+++ N-dimensional arrays - creating/GrayscaleImage.cs
@@ -0,0 +1,33 @@
+﻿public class GrayscaleImage
+{
+    public int Height { get; }
+    public int Width { get; }
+    //Set PixelData
+    public int[,] PixelData { get; }
+    public GrayscaleImage(int height, int width)
+    {
+        //Set Width, Height and PixelData
+        this.Height = height;
+        this.Width = width;
+        PixelData = new int[Height, Width];
+    }
+
+    public GrayscaleImage(int[,] pixelData)
+    {
+        //Set Width, Height and PixelData
+        PixelData = pixelData;
+        this.Height = pixelData.GetLength(0);
+        this.Width = pixelData.GetLength(1);
+    }
+
+    public void AdjustBrightness(double factor)
+    {
+        for (int i = 0; i < Height; i++)
+        {
+            for (int j = 0; j < Width; j++)
+            {
+                PixelData[i, j] = (int)((double)PixelData[i, j] * factor);
+            }
+        }
+    }
+}
