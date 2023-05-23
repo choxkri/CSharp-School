f6cbee17f498de5f2e50552c6a908bae7da4175a
choxkri <1059788@hr.nl>
2023-05-23 19:45:41 +0200

    finished HOF returning a lambda

---
+++ HOF returning a lambda/Point.cs
@@ -0,0 +1,18 @@
+﻿public class Point
+{
+    public double X { get; set; }
+    public double Y { get; set; }
+
+    public Point(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    // GetDistanceCalculator goes here
+    public static Func<Point, Point, double> GetDistanceCalculator(double factor)
+    {
+
+        return (p1, p2) => (Math.Sqrt(Math.Pow((double)(p1.X - p2.X), 2) + Math.Pow((double)(p1.Y - p2.Y), 2)) * factor);
+    }
+}
