b72029a07fea14b8c79b2dc20013fc196e3378a2
choxkri <1059788@hr.nl>
2023-05-23 22:39:04 +0200

    finished Historical Event Search

---
+++ Historical Event Search/HistoricalEvent.cs
@@ -0,0 +1,37 @@
+﻿public class HistoricalEvent
+{
+    public string Date { get; set; }
+    public string Description { get; set; }
+    public string Language { get; set; }
+    public string Category1 { get; set; }
+    public string Category2 { get; set; }
+    public string Granularity { get; set; }
+
+    public int Year
+    {
+        get
+        {
+            if (Date.Contains('/'))
+            {
+                string[] items = Date.Split('/');
+                return Convert.ToInt32(items[0]);
+            }
+
+            else
+            {
+                return Convert.ToInt32(Date);
+            }
+                
+        }
+    }
+
+    public HistoricalEvent(string date, string description, string language, string category1, string category2, string granularity)
+    {
+        Date = date;
+        Description = description;
+        Language = language;
+        Category1 = category1;
+        Category2 = category2;
+        Granularity = granularity;
+    }
+}
---
+++ Historical Event Search/HistoricalEventSearch.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+
+public static class HistoricalEventSearch
+{
+    public static List<HistoricalEvent> ReadEvents(string dataset)
+    {
+        StreamReader reader = new StreamReader(dataset);
+        string jsonString = reader.ReadToEnd();
+        reader.Close();
+        return JsonConvert.DeserializeObject<List<HistoricalEvent>>(jsonString)!;
+    }
+
+    public static List<HistoricalEvent> SearchInDescription(List<HistoricalEvent> data, string search)
+    {
+        List<HistoricalEvent> historicalEvents = new List<HistoricalEvent>();
+        var select = data.Where(x => x.Description.ToLower().Contains(search.ToLower()));
+        foreach (var item in select)
+        {
+            historicalEvents.Add(item);
+        }
+        return historicalEvents;
+    }
+
+    public static List<HistoricalEvent> SearchBetweenYears(List<HistoricalEvent> data, int yearbeg, int yearend)
+    {
+        List<HistoricalEvent> historicalEvents = new List<HistoricalEvent>();
+        var select = data.Where(x => yearbeg <= x.Year && x.Year <= yearend);
+        foreach (var item in select)
+        {
+            historicalEvents.Add(item);
+        }
+        return historicalEvents;
+    }
+
+    public static int AverageEventYear(List<HistoricalEvent> data)
+    {
+        int sum = 0;
+        foreach (HistoricalEvent ev in data) 
+        {
+            sum += ev.Year;
+        }
+        return sum / data.Count;
+    }
+}
