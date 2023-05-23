167a6ae3fa9495535d66611abb3ce63a026e0329
choxkri <1059788@hr.nl>
2023-05-23 14:45:12 +0200

    finished HOFS

---
+++ HOFs, Action/Company.cs
@@ -0,0 +1,16 @@
+﻿public class Company
+{
+    private List<Employee> _employees { get; } = new();
+    public void HireEmployee(Employee emp) => _employees.Add(emp);
+
+    // FilterEmployees goes here
+    public List<Employee> FilterEmployees(Func<Employee, bool> lam)
+    {
+        List<Employee> employees = new();
+        foreach(Employee emp in _employees.Where(lam))
+        {
+            employees.Add(emp);
+        }
+        return employees;
+    }
+}
