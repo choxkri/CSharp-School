4978071d407b7104c49ef64ad971d37a3c19c1f8
choxkri <1059788@hr.nl>
2023-05-23 15:03:10 +0200

    finished Lambda with multiple parameters

---
+++ Lambda with multiple parameters/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Give two numbers:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        PrintResult((x, y) => x + y, number1, number2);
+        PrintResult((x, y) => x - y, number1, number2);
+        PrintResult((x, y) => x * y, number1, number2);
+        PrintResult((x, y) => (int)Math.Pow(x, y), number1, number2);
+    }
+
+    // PrintResult goes here
+    public static void PrintResult(Func<int, int, int> calc, int n1, int n2)
+    {
+        Console.WriteLine($"The result is: {calc(n1, n2)}");
+    }
+}   
