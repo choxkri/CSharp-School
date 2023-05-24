b74153a328372a930af1b0979138959c2e564e55
choxkri <1059788@hr.nl>
2023-05-24 14:32:34 +0200

    finished filter

---
+++ Filter/HOF.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+public static class HOF
+{
+    // Filter goes here
+
+    public static List<T> Filter<T>(List<T> list, Func<T, bool> lam)
+    {
+        return list.Where(lam).ToList();
+    }
+
+
+}
