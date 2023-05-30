4a279cfae71ec51d4dde3907ffcbbd7304b6d54e
choxkri <1059788@hr.nl>
2023-05-30 22:43:19 +0200

    finished inventory system

---
+++ Inventory system/Inventory.cs
@@ -0,0 +1,40 @@
+﻿public class Inventory
+{
+    private Dictionary<string, Product> _products = new Dictionary<string, Product>();
+
+    public void AddProduct(string productName, double price, int quantity)
+    {
+        Product i = new(productName, price, quantity);
+        _products.Add(productName, i);
+    }
+
+    public void AddProduct(string productName, double price) 
+    {
+        Product i = new(productName, price, 0);
+        _products.Add(productName, i);
+    }
+
+    public void RemoveProduct(string productName) 
+    {
+        _products.Remove(productName);
+    }
+
+    public Product GetProduct(string productName)
+    {
+        return _products[productName];
+    }
+
+    public string GetInventorySummary()
+    {
+        int quant = 0;
+        double tprice = 0.0;
+
+        foreach (KeyValuePair<string, Product> kvp in _products)
+        {
+            quant += kvp.Value.Quantity;
+            tprice += kvp.Value.Quantity * kvp.Value.Price;
+        }
+
+        return $"Total products: {quant}\nTotal value: ${tprice}";
+    }
+}
---
+++ Inventory system/Product.cs
@@ -0,0 +1,18 @@
+﻿public class Product
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public int Quantity { get; set; }
+
+    public Product(string name, double price, int quantity)
+    {
+        Name = name;
+        Price = price;
+        Quantity = quantity;
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}, Price: ${Price}, Quantity: {Quantity}";
+    }
+}
