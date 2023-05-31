a36dc7609b79d40357a4f96d35a5e32373cff5fd
choxkri <1059788@hr.nl>
2023-05-31 14:58:27 +0200

    finished stack of integers

---
+++ Stack of integers/Program.cs
@@ -0,0 +1,46 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        //Create a stack of integers
+        Stack<int> stack = new Stack<int>();
+        //int to switch on
+        int choice;
+
+        do
+        {
+            Console.WriteLine("Menu:");
+            Console.WriteLine("1. Add integer to stack");
+            Console.WriteLine("2. Remove integer from stack");
+            Console.WriteLine("3. View top integer on stack");
+            Console.WriteLine("4. View all integers on stack");
+            Console.WriteLine("5. Exit program");
+            Console.Write("Enter the number of your choice: ");
+            choice = int.Parse(Console.ReadLine());
+
+            //Create a switch statement to handle the functions from the menu
+            switch (choice)
+            {
+                case 1:
+                    Console.WriteLine("integer:");
+                    int integer = int.Parse(Console.ReadLine());
+                    stack.Push(integer);
+                    break;
+                case 2:
+                    stack.Pop();
+                    break;
+                case 3:
+                    Console.WriteLine($"Top integer on stack: {stack.Peek()}");
+                    break;
+                case 4:
+                    Console.WriteLine("All integers on stack:");
+                    foreach (int i in stack)
+                    {
+                        Console.WriteLine(i);
+                    }
+                    break;
+            }
+        } while (choice != 5);
+    }
+}
+
