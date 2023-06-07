a8add07037feaa8ca1766a842887e6a07536023e
choxkri <1059788@hr.nl>
2023-06-07 15:31:13 +0200

    almost done with wijnhaven part 2

--- Wijnhaven (2 of 3)/ContainerLogger.cs
+++ Wijnhaven (2 of 3)/ContainerLogger.cs
@@ -1,4 +1,23 @@
 ﻿public static class ContainerLogger
 {
+    public static Dictionary<string, Container> containerLog = new Dictionary<string, Container>();
 
+    public static void Log(Container container)
+    {
+        if (!containerLog.ContainsKey(container.Code))
+        {
+            containerLog.Add(container.Code, container);
+            return;
+        }
+
+        containerLog[container.Code] = container;
+    }
+
+    public static void Print()
+    {
+        foreach (KeyValuePair<string, Container> kvp in containerLog)
+        {
+            Console.WriteLine(kvp.Value);
+        }
+    }
 }
--- Wijnhaven (2 of 3)/ContainerManager.cs
+++ Wijnhaven (2 of 3)/ContainerManager.cs
@@ -2,6 +2,7 @@
 
 public static class ContainerManager
 {
+    public static Queue<Container> selectedForInspection = new Queue<Container>();
     public static void Start(string path)
     {
         IEnumerable<string> allfiles = Directory.EnumerateFiles(path);
@@ -33,5 +34,37 @@ public static class ContainerManager
     {
         Container result = list.MaxBy(x => x.Weight);
         return result;
+    }
+
+    public static void MarkForInspection(List<Container> list)
+    {
+        foreach (Container cont in list)
+        {
+            cont.Status = 9;
+            ContainerLogger.Log(cont);
+        }
+        Console.WriteLine($"Number of containers selected for inspection: {list.Count}");
+    }
+
+    public static void MarkForInspection(List<Container> list, Func<Container, bool> filter)
+    {
+        int count = 0;
+        foreach (Container cont in list)
+        {
+            if (filter(cont))
+            {
+                cont.Status = 1;
+                selectedForInspection.Enqueue(cont);
+                ContainerLogger.Log(cont);
+                count++;
+            }
+
+            else
+            {
+                cont.Status = 9;
+                ContainerLogger.Log(cont);
+            }
+        }
+        Console.WriteLine($"Number of containers selected for inspection: {count}");
     }
 }
