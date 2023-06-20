9e0b40cda325a385960e60cc6c81020f4a0e8ba3
choxkri <1059788@hr.nl>
2023-06-20 23:19:43 +0200

    finished Most frequent character

---
+++ Most frequent character/Program.cs
@@ -0,0 +1,10 @@
+﻿string input = Console.ReadLine();
+
+var query = input.GroupBy(x => x).OrderByDescending(x => x.Count()).ToList();
+
+foreach (var item in query)
+{
+    Console.WriteLine(item.Key);
+    Environment.Exit(0);
+}
+
