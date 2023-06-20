98f1e751fc8cc12440bf9c17d72dabbed902d7c1
choxkri <1059788@hr.nl>
2023-06-20 22:29:12 +0200

    finished Character counter

---
+++ Character counter/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Input string: ");
+string str = Console.ReadLine();
+List<char> chars = new List<char>();
+foreach (char character in str)
+{
+    chars.Add(character);
+}
+var list = str.Distinct();
+foreach (var item in list)
+{
+    char current = item;
+    var occurences = from character in chars
+                     where character == current
+                     select character;
+    Console.WriteLine($"Char [{current}]: {occurences.Count()}");
+}
