86afbc1d89a44207b54064d3c98e1ffcc4ded8bd
choxkri <1059788@hr.nl>
2023-06-21 15:51:47 +0200

    finished sample exams

--- Sample Exam OP4 Assignment 4/Program.cs
+++ Sample Exam OP4 Assignment 4/Program.cs
@@ -1,2 +1,71 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public enum Course
+{
+    Django,
+    OODP,
+    Project
+}
+
+
+public class StudentEnrollmentSystem
+{
+    public Dictionary<Course, Queue<Student>> waitlist;
+    public Dictionary<Course, List<Student>> enrolledStudents;
+
+    public StudentEnrollmentSystem()
+    {
+        waitlist = new Dictionary<Course, Queue<Student>>
+        {
+            {Course.Django, new Queue<Student>()},
+            {Course.OODP, new Queue<Student>()},
+            {Course.Project, new Queue<Student>()}
+        };
+
+        enrolledStudents = new Dictionary<Course, List<Student>>
+        {
+            {Course.Django, new List<Student>()},
+            {Course.OODP, new List<Student>()},
+            {Course.Project, new List<Student>()}
+        };
+    }
+
+    public void EnrollStudent(string name, string id, Course course)
+    {
+        Student stu = new(id, name);
+        waitlist[course].Enqueue(stu);
+    }
+
+    public string CheckStatus(string id)
+    {
+        foreach (var pair in waitlist) 
+        {
+            var test = pair.Value.Where(x => x.ID == id).ToList();
+            if (!(test.Count == 0))
+            {
+                return $"Student with {test[0].ID} is in the waitlist for {pair.Key} course.";
+                
+            }
+        }
+
+        foreach (var pair in enrolledStudents)
+        {
+            var test = pair.Value.Where(x => x.ID == id).ToList();
+            if (!(test.Count == 0))
+            {
+                return $"Student with {test[0].ID} enrolled in {pair.Key} course.";
+            }
+        }
+
+        return $"Student with ID {id} not found.";
+    }
+
+    public void EnrollNextStudent(Course course)
+    {
+        Student enrolling = waitlist[course].Dequeue();
+        enrolledStudents[course].Add(enrolling);
+    }
+
+    public List<Student> GetEnrolledStudentsByCourse(Course course)
+    {
+        return enrolledStudents[course];
+    }
+}
---
+++ Sample Exam OP4 Assignment 4/Student.cs
@@ -0,0 +1,14 @@
+﻿public class Student
+{
+
+    public string ID { get; set; }
+    public string Name { get; set; }
+
+
+    public Student(string id, string name)
+    {
+        Name = name;
+        ID = id;
+    }
+
+}
